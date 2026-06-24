3fa1382b42f23143cde9ccbd5753664c0b27dc58
Jose-Murilo <murilojucas360@hotmail.com>
2026-06-24 18:05:31 -0300

    feat: adding new configs in project

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { pessoaRoutes } from "./routes/pessoa.routes";
+import { animalRoutes } from "./routes/animal.routes";
+import path from "path";
+import { postRoutes } from "./routes/post.routes";
+import { comentarioRoutes } from "./routes/comentario.routes";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  return res.json({
+    mensagem: "API Blog Animal com POO funcionando!",
+  });
+});
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use("/pessoas", pessoaRoutes);
+app.use("/animais", animalRoutes);
+app.use(
+  "/uploads",
+  express.static(
+    path.resolve(process.cwd(), "uploads")
+  )
+);
+app.use("/posts", postRoutes);
+app.use("/comentarios", comentarioRoutes);
+
+export { app };
---
+++ src/config/multer.ts
@@ -0,0 +1,29 @@
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+import crypto from "crypto";
+
+const uploadPath = path.resolve("uploads", "animals");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, uploadPath);
+  },
+
+  filename: (_req, file, cb) => {
+    const hash = crypto.randomBytes(10).toString("hex");
+
+    cb(
+      null,
+      `${hash}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+export const upload = multer({
+  storage,
+});
